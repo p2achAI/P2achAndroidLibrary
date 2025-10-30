e38720ad192dd7040f91cccc2ab5dab1e0bd9c38
seehoon <flytofree@p2ach.ai>
2025-10-30 13:29:54 +0900

    autoBinding 추가

--- p2achAndroidLibrary/src/main/java/ai/p2ach/p2achandroidlibrary/base/fragments/BaseFragment.kt
+++ p2achAndroidLibrary/src/main/java/ai/p2ach/p2achandroidlibrary/base/fragments/BaseFragment.kt
@@ -7,27 +7,35 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import java.lang.reflect.ParameterizedType
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 /*
 * BaseActivity와 마찬가지로 선언된 VB를 자동으로 inflate하여 자식에서의 Boilerplate Code 방지
 * */
 
-abstract class BaseFragment<VB : ViewBinding>: Fragment() {
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
-    private var fragmentBinding: VB? = null
-    protected val _fragmentBinding get() = fragmentBinding!!
+    private var _binding: VB by autoCleared()
+    protected val binding: VB
+        get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        if (fragmentBinding == null) {
-            val vbClass = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<VB>
-            val inflateMethod = vbClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
-            fragmentBinding = inflateMethod.invoke(null, inflater, container, false) as VB
-        }
-        return fragmentBinding!!.root
+    ): View {
+
+        val vbClass = (javaClass.genericSuperclass as ParameterizedType)
+            .actualTypeArguments[0] as Class<VB>
+        val inflate = vbClass.getMethod(
+            "inflate",
+            LayoutInflater::class.java,
+            ViewGroup::class.java,
+            Boolean::class.java
+        )
+        _binding = inflate.invoke(null, inflater, container, false) as VB
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,5 +43,36 @@ abstract class BaseFragment<VB : ViewBinding>: Fragment() {
         viewInit(savedInstanceState)
     }
 
-    open fun viewInit(savedInstanceState: Bundle?){}
+    open fun viewInit(savedInstanceState: Bundle?) {}
+
+    protected inline fun autoBinding(block: VB.() -> Unit) {
+        binding.block()
+    }
+}
+
+
+fun <T : Any> Fragment.autoCleared() = AutoClearedValue<T>(this)
+
+class AutoClearedValue<T : Any>(fragment: Fragment) : ReadWriteProperty<Fragment, T> {
+    private var _value: T? = null
+
+    init {
+        fragment.viewLifecycleOwnerLiveData.observe(fragment) { owner ->
+            owner?.lifecycle?.addObserver(
+                object : androidx.lifecycle.DefaultLifecycleObserver {
+                    override fun onDestroy(owner: androidx.lifecycle.LifecycleOwner) {
+                        _value = null
+                    }
+                }
+            )
+        }
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        return _value ?: error("Binding accessed outside of view lifecycle.")
+    }
+
+    override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
+        _value = value
+    }
 }
